aae241b5f1ef53762029025a07c4c01a50287be4
Jeff <retroh3at@gmail.com>
2015-12-03 15:59:22 -0500

    added socket

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var PORT = process.env.PORT || 3000;
 var express = require('express'); 
 var app = express(); 
 var http = require('http').Server(app); 
+var io = require('socket.io')(http); 
 
 app.use(express.static(__dirname + '/public')); 
+
+io.on('connection', function () {
+	console.log('Client has connected'); 
+})
 
 http.listen(PORT, function(){
 	console.log('server started on port:' + PORT)
